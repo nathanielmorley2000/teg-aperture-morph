ccb9efc758f1be4af7d49b0e37e627ae775a64bd
nathanielmorley2000 <nathaniel.morley2000@gmail.com>
2025-07-15 13:42:40 -0600

    Update teg-aperture-morph.R
    Loaded characteristics list into code and added commentary.

--- teg-aperture-morph.R
+++ teg-aperture-morph.R
@@ -1,3 +1,8 @@
+# load libraries
 library("Momocs")
 
+# load characteristics list
+characteristics <- read.csv("Data/TegulaCharacteristicsList.csv")
+
+# begin eliptical fourier analysis
 import_jpg1("Data/Silhouettes/PrasiolaHighEnergy/PrasTeg50.jpg")
