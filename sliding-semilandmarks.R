9fa5d8f9a0eb1b0ec38419d778630e7263f7af8d
nathanielmorley2000 <nathaniel.morley2000@gmail.com>
2025-08-19 14:13:00 -0600

    Update sliding-semilandmarks.R
    Code changed to reference TegulaOutlines - Copy.TPS and accept curve. Uploading semi-landmarks correctly.

--- sliding-semilandmarks.R
+++ sliding-semilandmarks.R
@@ -2,8 +2,8 @@
 library("geomorph")
 
 # Upload TPS file with semi-landmark data
-landmarks <- readland.tps(file = "Data/TegulaOutlines.TPS", specID = "ID", negNA = FALSE,
-                          readcurves = FALSE, warnmsg = TRUE)
+landmarks <- readland.tps(file = "Data/TegulaOutlines - Copy.TPS", specID = "ID", negNA = FALSE,
+                          readcurves = TRUE, warnmsg = TRUE)
 
 # Upload specimen characteristics data
 characteristics <- read.csv("Data/TegulaCharacteristicsList.csv")
