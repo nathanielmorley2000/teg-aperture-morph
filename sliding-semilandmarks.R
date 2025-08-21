650ef1502ddd4edefda1049c4ce77b0dab55c4c9
nathanielmorley2000 <nathaniel.morley2000@gmail.com>
2025-08-21 10:46:22 -0600

    Update sliding-semilandmarks.R
    Changed sliders to be compatible with gpagen() command. Experimenting with different Procrustes techniques and plotting a PCA.

--- sliding-semilandmarks.R
+++ sliding-semilandmarks.R
@@ -9,4 +9,26 @@ landmarks <- readland.tps(file = "Data/TegulaOutlines - Copy.TPS", specID = "ID"
 characteristics <- read.csv("Data/TegulaCharacteristicsList.csv")
 
 # Upload sliders file
-sliders <- read.csv("Data/Sliders.csv", header = FALSE)
+curves <- as.matrix(read.csv("Data/Sliders.csv", header=T))
+
+# Using Procrustes Distance for sliding
+A <- gpagen(landmarks,
+            curves = curves,
+            ProcD = TRUE, print.progress = TRUE)
+
+# Using bending energy for sliding
+B <- gpagen(landmarks,
+            curves = curves,
+            ProcD = FALSE, print.progress = FALSE)
+
+summary(A)
+summary(B)
+
+
+plotTangentSpace(A, axis1 = 1, axis2 = 2, warpgrids = TRUE, mesh = NULL, label = FALSE,
+                 groups = NULL, legend = FALSE)
+
+# Convert semi-landmark data into a 2-d array
+twod_landmarks <- two.d.array(landmarks)
+
+
