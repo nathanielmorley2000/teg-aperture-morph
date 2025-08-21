63a2b9a15d9a9451fa63c20746597f8ed43f39da
nathanielmorley2000 <nathaniel.morley2000@gmail.com>
2025-08-21 12:20:22 -0600

    Update sliding-semilandmarks.R
    Remove characteristics for specimens without outlines.

--- sliding-semilandmarks.R
+++ sliding-semilandmarks.R
@@ -14,6 +14,9 @@ named.landmarks <- arrayspecs(two.d.landmarks, 90, 2)
 # Upload specimen characteristics data
 characteristics <- read.csv("Data/TegulaCharacteristicsList.csv", row.names = 1)
 
+# Subset characteristics to only include those that were used in analysis
+specific.characteristics <- characteristics[row.names(characteristics) %in% rownames(two.d.landmarks),]
+
 # Upload sliders file
 curves <- as.matrix(read.csv("Data/Sliders.csv", header=T))
 
