df7683cff02f8228250f684e8e11ae611a3d81d8
nathanielmorley2000 <nathaniel.morley2000@gmail.com>
2025-08-22 17:21:33 -0600

    Update sliding-semilandmarks.R
    Adding code for saving GPA results as dataframe and for creating a PCA plot with groups delineated based on energy.

--- sliding-semilandmarks.R
+++ sliding-semilandmarks.R
@@ -1,6 +1,7 @@
 # Call packages
 library("geomorph")
 library("stringr")
+#library("ggplot2")
 
 # Upload TPS file with semi-landmark data
 landmarks <- readland.tps(file = "Data/TegulaOutlines.TPS", specID = "imageID", negNA = FALSE,
@@ -27,8 +28,47 @@ Procrustes <- gpagen(named.landmarks,
 plot(Procrustes)
 summary(Procrustes)
 
+# Save GPA results to dataframe
+GPA.Results <- geomorph.data.frame(Procrustes, 
+                                   location = as.factor(specific.characteristics$Location),
+                                   energy = as.factor(specific.characteristics$Energy.Setting),
+                                   repairs = as.factor(specific.characteristics$Repair.Scars..0.1.))
+
+
 # Create PCA plot
-PCA <- gm.prcomp(Procrustes$coords)
+PCA <- gm.prcomp(GPA.Results$coords)
+PCA.Energy <- plot(PCA, main = "PCA", col = GPA.Results$energy, theme = classic)
+shapeHulls(PCA.Energy, groups = GPA.Results$energy,
+           group.cols = 1:3, 
+           group.lwd = c(3,3,3))
+legend("bottomright", c("High", "Low", "Moderate"), 
+       col = 1:3, lwd = 2)
+
+
+pc.plot <- plot(pleth.phylo, phylo = TRUE)
+gp <- factor(c(rep(1, 5), rep(2, 4)))
+shapeHulls(pc.plot, groups = gp, group.cols = 1:2, 
+           group.lwd = rep(2, 2), group.lty = c(2, 1))
+legend("topright", c("P. cinereus clade", "P. hubrichti clade"), 
+       col = 1:2, lwd = 2, lty = c(2, 1))
+
+
+
+
+
+
+
+
+
+
+
+plot(PCA, main = "PCA", col = GPA.Results$repairs)
+
+
+PCA.Plot <- plot(PCA, main = "PCA")
+picknplot.shape(PCA.Plot)
+
+
 summary(PCA)
 plot(PCA, main = "PCA")
 plot(PCA, main = "PCA", flip = 1) # flip the first axis
