b0c0b298e8a039d5d03c7f54d600956460f1a652
nathanielmorley2000 <156269155+nathanielmorley2000@users.noreply.github.com>
2025-11-24 18:18:46 -0700

    Update sliding-semilandmarks.R
    Added code needed to make PCA with scree plot and PCs 1, 2, and 3.

--- sliding-semilandmarks.R
+++ sliding-semilandmarks.R
@@ -1,7 +1,10 @@
 # Call packages
 library("geomorph")
 library("stringr")
+library("dplyr")
+library("tidyr")
 library("ggplot2")
+library("gridExtra")
 
 ############################## Generalized Procrustes Analysis ##############################
 
@@ -47,9 +50,77 @@ GPA.Results <- geomorph.data.frame(Procrustes,
 
 # Produce simple PCA (no size correction)
 PCA <- gm.prcomp(GPA.Results$coords)
-plot(PCA, col = GPA.Results$location)
-legend("bottomright", legend = levels(GPA.Results$location), 
-       col = 1:3, pch = 16)
+summary(PCA)
+
+# First three PCs explain the most variation. Create dataframe with first three PCs
+PCA_Results <- data.frame(Location = specific.characteristics$Location,
+                          RepairScars = specific.characteristics$Repair.Scars..0.1.,
+                          height = specific.characteristics$Height..mm.,
+                          PC1 = PCA$x[,1],
+                          PC2 = PCA$x[,2],
+                          PC3 = PCA$x[,3])
+
+# Make scree plot
+prop <- data.frame(as.list(PCA$d/sum(PCA$d))) %>%
+  pivot_longer(cols = 1:89)
+
+scree_plot <- ggplot(data = prop[1:9,], aes(x = name, y = value)) +
+  geom_col(fill = "blue3") +
+  xlab("Principal Component") +
+  ylab("Proportion of Variance Explained") +
+  ggtitle("Scree Plot") +
+  theme_test()
+scree_plot
+
+# create function to calculate the convex hull for each group
+find_hull <- function(df) df[chull(df$PC1, df$PC2), ]
+
+# apply the function to your PCA scores grouped by your factor
+PCA_Hulls2 <- PCA_Results %>%
+  group_by(Location) %>%
+  do(find_hull(.))
+
+# Plot PC Axes 1 and 2
+PCA_Plot2 <- ggplot() +
+  geom_polygon(data = PCA_Hulls2, aes(x = PC1,
+                                     y = PC2, 
+                                     fill = Location), alpha = 0.2) + # Convex hulls 
+  geom_point(data = PCA_Results, aes(x = PC1,
+                                   y = PC2,
+                                   color = Location,
+                                   shape = Location), size = 3) + # Points
+  xlab("PC1: 32.9%") +
+  ylab("PC2: 15.2%") +
+  theme_test()
+PCA_Plot2
+
+
+# create function to calculate the convex hull for each group
+find_hull <- function(df) df[chull(df$PC1, df$PC3), ]
+
+# apply the function to your PCA scores grouped by your factor
+PCA_Hulls3 <- PCA_Results %>%
+  group_by(Location) %>%
+  do(find_hull(.))
+
+# Plot PC Axes 1 and 3
+PCA_Plot3 <- ggplot() +
+  geom_polygon(data = PCA_Hulls3, aes(x = PC1,
+                                      y = PC3, 
+                                      fill = Location), alpha = 0.2) + # Convex hulls 
+  geom_point(data = PCA_Results, aes(x = PC1,
+                                     y = PC3,
+                                     color = Location,
+                                     shape = Location), size = 3) + # Points
+  xlab("PC1: 32.9%") +
+  ylab("PC3: 11.7%") +
+  theme_test()
+
+# Compile master plot with PCs and scree plot
+master_plot <- grid.arrange(scree_plot, PCA_Plot2, PCA_Plot3, layout_matrix = matrix(c(1, 2, 1, 3), nrow = 2))
+ggsave("PCA.png", master_plot, height = 9, width = 18, units = "in")
+
+
 
 # Produce simple allometry PCA
 fit <- procD.lm(coords ~ log(height), data = GPA.Results,
