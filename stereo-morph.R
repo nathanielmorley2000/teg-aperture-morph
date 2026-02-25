8cd71d96885e5324744388a092870b70d8318dd2
nathanielmorley2000 <156269155+nathanielmorley2000@users.noreply.github.com>
2026-02-25 14:20:06 -0700

    Remove old code
    Removed old code files for using elliptical Fourier analysis.

--- stereo-morph.R
+++
@@ -1,5 +0,0 @@
-# load package
-library("StereoMorph")
-
-# launch StereoMorph application
-digitizeImages(image.file='Data/PrasiolaHighEnergy/', shapes.file='Data/Shapes/', curves.ref = "curves.txt")
