0dba78f01327973cfe4c41dcc0d46baf57314188
nathanielmorley2000 <156269155+nathanielmorley2000@users.noreply.github.com>
2026-03-12 01:17:32 -0600

    Update sliding-semilandmarks.R
    Added code to change colour palette per Chris' suggestion. Also changed shapes among localities for extra visual cue.

--- sliding-semilandmarks.R
+++ sliding-semilandmarks.R
@@ -82,13 +82,16 @@ interactive_PCA <- plot_ly(data = PCA_Results,
                            y = ~PC2,
                            z = ~PC3,
                            color = ~Location,
-                           colors = c("#fc8d62", "#66c2a5", "#8da0cb"),
-                           marker = list(size = 7),
+                           colors = c("#e7192b", "#22407c", "#58a144"),
+                           symbol = ~Location,
+                           symbols = c(18, 19, 15),
+                           marker = list(size = 5),
                            type = "scatter3d",
                            mode = "markers") %>%
   layout(legend = list(orientation = "h",
                        xanchor = "center",
-                       x = 0.5), 
+                       x = 0.5,
+                       itemsizing = "constant"), 
          scene = list(xaxis = list(title = "PC1 (32.9%)",
                                    dtick = 0.05,
                                    tickformat = ".2f",
@@ -117,10 +120,11 @@ orca(static_PCA, "Results/MorphologyResults/Static_PCA.svg", scale = 1.5)
 
 
 # Plot PC1 individually
-PC1_Plot <- ggplot(PCA_Results, aes(x = PC1, y = "", color = Location)) +
-  geom_point(position = position_jitter(width = 0, height = 0.15), size = 3) +
+PC1_Plot <- ggplot(PCA_Results, aes(x = PC1, y = "", color = Location, shape = Location)) +
+  geom_point(position = position_jitter(width = 0, height = 0.15), size = 3.5) +
   scale_x_continuous(breaks = seq(-0.10, 0.15, by = 0.05)) +
-  scale_color_manual(values = c("Strawberry" = "#fc8d62", "Mathers" = "#66c2a5", "Prasiola" = "#8da0cb")) +
+  scale_color_manual(values = c("Strawberry" = "#e7192b", "Mathers" = "#22407c", "Prasiola" = "#58a144")) +
+  scale_shape_manual(values = c("Strawberry" = 18, "Mathers" = 19, "Prasiola" = 15)) +
   labs(title = "B", x = "PC1 (32.9%)", y = "") +
   theme_classic() +
   theme(legend.position = "none",
@@ -129,10 +133,11 @@ PC1_Plot <- ggplot(PCA_Results, aes(x = PC1, y = "", color = Location)) +
 ggsave("Results/MorphologyResults/PC1.svg", PC1_Plot, width = 10, height = 2) # Save 1D plot
 
 # Plot PC2 individually
-PC2_Plot <- ggplot(PCA_Results, aes(x = PC2, y = "", color = Location)) +
-  geom_point(position = position_jitter(width = 0, height = 0.15), size = 3) +
+PC2_Plot <- ggplot(PCA_Results, aes(x = PC2, y = "", color = Location, shape = Location)) +
+  geom_point(position = position_jitter(width = 0, height = 0.15), size = 3.5) +
   scale_x_continuous(breaks = seq(-0.10, 0.10, by = 0.05)) +
-  scale_color_manual(values = c("Strawberry" = "#fc8d62", "Mathers" = "#66c2a5", "Prasiola" = "#8da0cb")) +
+  scale_color_manual(values = c("Strawberry" = "#e7192b", "Mathers" = "#22407c", "Prasiola" = "#58a144")) +
+  scale_shape_manual(values = c("Strawberry" = 18, "Mathers" = 19, "Prasiola" = 15)) +
   labs(title = "C", x = "PC2 (15.2%)", y = "") +
   theme_classic() +
   theme(legend.position = "none",
@@ -141,10 +146,11 @@ PC2_Plot <- ggplot(PCA_Results, aes(x = PC2, y = "", color = Location)) +
 ggsave("Results/MorphologyResults/PC2.svg", PC2_Plot, width = 10, height = 2) # Save 1D plot
 
 # Plot PC3 individually
-PC3_Plot <- ggplot(PCA_Results, aes(x = PC3, y = "", color = Location)) +
-  geom_point(position = position_jitter(width = 0, height = 0.15), size = 3) +
+PC3_Plot <- ggplot(PCA_Results, aes(x = PC3, y = "", color = Location,  shape = Location)) +
+  geom_point(position = position_jitter(width = 0, height = 0.15), size = 3.5) +
   scale_x_continuous(breaks = seq(-0.10, 0.10, by = 0.05)) +
-  scale_color_manual(values = c("Strawberry" = "#fc8d62", "Mathers" = "#66c2a5", "Prasiola" = "#8da0cb")) +
+  scale_color_manual(values = c("Strawberry" = "#e7192b", "Mathers" = "#22407c", "Prasiola" = "#58a144")) +
+  scale_shape_manual(values = c("Strawberry" = 18, "Mathers" = 19, "Prasiola" = 15)) +
   labs(title = "D", x = "PC3 (11.7%)", y = "") +
   theme_classic() +
   theme(legend.position = "none",
